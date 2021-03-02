21500fe5dc0f2e885ad5d7fe2a647940dfb84832
anastasiia-kolieva <nastya1110@gmail.com>
2021-03-02 10:31:06 +0200

    corrected listContacts, getContactById, removeContact, addContact, updateContact up to Mongoose

--- model/index.js
+++ model/index.js
@@ -1,31 +1,9 @@
-const fs = require("fs").promises;
-const path = require("path");
-// ?????????????????????????????????????????????????????????????????
-// shortId удалила с зависимостей!!!!
-const db = require("./db");
-
-// const getCollection= async (db, name)=>{
-//   const client = await db;
-//   const collection = await client.collection.db(name);
-//   return collection;
-// }
-// //???????????????????????????????????????????????????????????????????
-
-const { readData, parceData, writeData } = require("../helpers/helpers.js");
-
-const contactsPath = path.join(__dirname, "/contacts.json");
+const Contact = require("./schemas/contact");
 
 const listContacts = async () => {
   try {
-// //???????????????????????????????????????????????????????????????????
-    // const collection = await getCollection(db,'contacts');
-    // //???????????????????????????????????????????????????????????????????
-
-    // данные с прочитки файла contacts.json
-    const data = await readData(contactsPath);
-
-    // JSON.parse необходимо чтоб распарсить в обьект, потому что там строка сформированна
-    return parceData(data);
+    const data = await Contact.find({});
+    return data;
   } catch (error) {
     console.log(error);
   }
@@ -33,12 +11,8 @@ const listContacts = async () => {
 
 const getContactById = async (contactId) => {
   try {
-    const data = await readData(contactsPath);
-    const parseData = parceData(data);
-
-    //в данных с прочитки файла contacts.json ищем необходимый контакт по id
-    const contact = parseData.find((contact) => contact.id == contactId);
-    return contact;
+    const data = Contact.find({ _id: contactId });
+    return data;
   } catch (error) {
     console.log(error);
   }
@@ -46,40 +20,16 @@ const getContactById = async (contactId) => {
 
 const removeContact = async (contactId) => {
   try {
-    const data = await readData(contactsPath);
-    const parseData = parceData(data);
-    const deletedContact = parseData.find((contact) => contact.id == contactId);
-
-    const filteredContacts = parseData.filter(
-      (contact) => contact.id != contactId
-    );
-
-    if (filteredContacts.length !== parseData.length) {
-      // если отфильтрованный массив контактов НЕ равен изначальному массиву, значит искомый контакт убрали
-      // перезаписываем файл (путьcontactsPath ) с контактами. В него записываем масив filteredContacts
-      // await fs.writeFile(contactsPath, JSON.stringify(filteredContacts));
-      await writeData(contactsPath, filteredContacts);
-      return deletedContact;
-    } else {
-      return;
-    }
+    const deletedContact = Contact.findOneAndRemove({ _id: contactId });
+    return deletedContact;
   } catch (error) {
     console.log(error);
   }
 };
 
-// c req.body приходит name, email, phone нового контакта
-const addContact = async ({ name, email, phone }) => {
+const addContact = async (body) => {
   try {
-    const data = await readData(contactsPath);
-    const parseData = parceData(data);
-    const newContact = { id: shortid.generate(), name, email, phone };
-
-    parseData.push(newContact);
-    // перезаписываем файл (путь contactsPath ) с контактами. В него записываем масив parseData
-    // await fs.writeFile(contactsPath, JSON.stringify(parseData));
-    await writeData(contactsPath, parseData);
-    // отправляем новый созданный контакт
+    const newContact = await Contact.create(body);
     return newContact;
   } catch (error) {
     console.log(error);
@@ -88,22 +38,12 @@ const addContact = async ({ name, email, phone }) => {
 
 const updateContact = async (contactId, body) => {
   try {
-    const data = await readData(contactsPath);
-    // данные с прочитки файла contacts.json
-    const parseData = parceData(data);
-    //в данных с прочитки файла contacts.json ищем необходимый контакт по id
-    const contact = parseData.find((contact) => contact.id == contactId);
-    // в обновлённый контакт(updatedContact) распыляем свойства найденного по id контакта,
-    // и поверх приходящие новые значения body
-    const updatedContact = { ...contact, ...body };
-
-    const filteredContacts = parseData.filter(
-      (contact) => contact.id != contactId
+    const updatedContact = await Contact.findByIdAndUpdate(
+      { _id: contactId },
+      { ...body },
+      // новое значение
+      { new: true }
     );
-
-    const updatedArrayOfContacts = [updatedContact, ...filteredContacts];
-    // await fs.writeFile(contactsPath, JSON.stringify(updatedArrayOfContacts));
-    await writeData(contactsPath, updatedArrayOfContacts);
     return updatedContact;
   } catch (error) {
     console.log(error);
--- model/schemas/contact.js
+++ model/schemas/contact.js
@@ -1,33 +1,39 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Set name of contact"],
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name of contact"],
+    },
+    email: {
+      type: String,
+      required: [true, "Set email of contact"],
+      unique: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    subscription: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: [true, "Set password"],
+    },
+    token: {
+      type: String,
+      default: "",
+    },
   },
-  email: {
-    type: String,
-    required: [true, "Set email of contact"],
-    unique: true,
-  },
-  phone: {
-    type: Number,
-    required: true,
-    unique: true,
-  },
-  subscription: {
-    type: String,
-  },
-  password: {
-    type: String,
-    required: [true, "Set password"],
-  },
-  token: {
-    type: String,
-    default: "",
-  },
-});
+  // отключить вывод версий
+  { versionKey: false, 
+    // 
+    timestamps:true }
+);
 
 const Contact = model("contact", contactSchema);
 
