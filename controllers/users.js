7034bf645feff43db2f74ce3061b07d71cf24e19
anastasiia-kolieva <nastya1110@gmail.com>
2021-03-07 21:13:42 +0200

    added validation of user by email

--- controllers/users.js
+++ controllers/users.js
@@ -7,6 +7,22 @@ const SECRET_KEY = process.env.JWT_SECRET;
 
 const registration = async (_req, res, next) => {
   try {
+    //  пришло от заполнения формы
+    const { name, email, password, subscription } = req.body;
+    // перед тем как регестрировать пользователя, его нужно найти (юзера)
+    const user = await users.findByEmail(email);
+    // если пользователь есть (конфликт имен)
+    // Если почта уже используется кем-то другим, вернуть Ошибку Conflict.
+    if (user) {
+      // сообщаем о конфликте
+      return req.status(HttpCode.CONFLICT).json({
+        status: "error",
+        code: HttpCode.CONFLICT,
+        data: "Conflict",
+        message: "Email in use",
+      });
+    }
+
     // В responce массив обектов контактов распарсенный
     const responce = await contacts.listContacts();
     return res.json({
@@ -23,5 +39,5 @@ const registration = async (_req, res, next) => {
 };
 
 module.exports = {
-    registration
-  };
+  registration,
+};
